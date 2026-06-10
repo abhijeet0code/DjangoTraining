24c5344d125bb4ca1044925ccffa6a9e4f61e030
Abhijeet Singh <abhijeetsingh0code@gmail.com>
2026-06-10 19:05:20 +0530

    Basic leap year program

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+year=int(input("Enter a number:"))
+
+
+if year % 400==0:
+    print("It's a leap year")
+else:
+    if year%4==0 and year%100!=0:
+        print("It's a leap year")
+    else:
+        print("It's a leap year")
+
