71f6fdd0de369884bd81e09ec9d6286100de99d9
Abhijeet Singh <abhijeetsingh0code@gmail.com>
2026-06-30 12:27:38 +0530

    Inheritence Concept

---
+++ multi_levelInheritence.py
@@ -0,0 +1,12 @@
+class a:
+    def __init__(self):
+        print("This is the constructor of the a")
+class b:
+    def __init__(self):
+        print("This is the constructor of the b")
+class c:
+    def __init__(self):
+        print("This is the constructor of the c")
+
+ob=c()
+print(c)
