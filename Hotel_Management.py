aab3b4caf18e2845aaebb562bde7afaed083e384
Abhijeet Singh <abhijeetsingh0code@gmail.com>
2026-07-12 10:18:38 +0530

    Made the menu section and food isnert section to with the exit and more buttons to follow

--- Hotel_Management.py
+++ Hotel_Management.py
@@ -1,8 +1,11 @@
+from click import command
 from customtkinter import *
 from tkinter import messagebox #Imported to display some warnings
 from CTkToolTip import CTkToolTip #Imported for the hover message
 import pymysql
 from PIL import Image, ImageTk
+from unicodedata import category
+
 
 #------------- Data Base for the SQL -------------------
 def db_config():
@@ -14,15 +17,25 @@ def db_config():
 
 
 #------------------- Custom Tkinter ----------------------
+menu_frame=None
 taz=CTk()
 taz.title("Hotel Management System")
 #--------------------------------------------------------
 
 #------------------------ Data Base Variable--------------------------------
+#------------------------- Info Variable-------------------------------
+
 usernameVar=StringVar()
 passwordVar=StringVar()
 emailVar=StringVar()
 confirm_passVar=StringVar()
+#------------------------ Item insert var--------------------------------
+
+itemnameVar=StringVar()
+hpriceVar=StringVar()
+fpriceVar=StringVar()
+selectOptionVar=StringVar()
+
 #--------------------------------------------------------
 
 
@@ -43,8 +56,8 @@ def hotel_heading():
         font=("Arial", 28, "bold"),
         text_color="white"
     )
-    heading.place(x=600,y=20)
-hotel_heading()
+    heading.place(relx=0.5,rely=0.5,anchor="center")
+
 #--------------------------------------------------------
 
 
@@ -71,22 +84,41 @@ def switch_option(switch_to):
 
 
 #----------------------- Creating a page frame for the login and signup-----------------------
+def login_frame():
+    global page_frame
+    page_frame = CTkFrame(master=taz, width=800, height=550, corner_radius=10)
+    page_frame.place(relx=0.5, rely=0.55, anchor="center")
 
-page_frame=CTkFrame(master=taz,width=800,height=550,corner_radius=10)
-page_frame.place(x=370,y=190)
+
 #--------------------------------------------------------
 
 
 #----------------- Login Button --------------------
 
-login_page_btn=CTkButton(master=taz,text='Login',font=('Arial',20,'bold'),text_color='white',width=120,height=40,
-                         corner_radius=10,border_width=2,
-                         border_color='white',fg_color='#1F6AA5',command=lambda: switch_option(switch_to='login'))
-login_page_btn.place(x=600,y=100)
+def login_sign():
+    global login_page_btn,signup_page_btn
+    login_page_btn = CTkButton(master=taz, text='Login', font=('Arial', 20, 'bold'), text_color='white', width=120,
+                               height=40,
+                               corner_radius=10, border_width=2,
+                               border_color='white', fg_color='#1F6AA5',
+                               command=lambda: switch_option(switch_to='login'))
+    login_page_btn.place(relx=0.45, rely=0.148, anchor="center")
+
+    signup_page_btn = CTkButton(master=taz, text='Sign-Up', font=('Arial', 20, 'bold'), text_color='white', width=120,
+                                height=40,
+                                corner_radius=10, border_width=2,
+                                border_color='white', fg_color='#1F6AA5',
+                                command=lambda: switch_option(switch_to='signup'))
+    signup_page_btn.place(relx=0.58, rely=0.148, anchor="center")
+
+
 #--------------------------------------------------------
 
 #--------------------------Login Page ------------------------------
 def login_page():
+    hotel_heading()
+    login_sign()
+    login_frame()
     signup_page_btn.configure(fg_color="gray")
     heading=CTkLabel(master=page_frame, text="Login Page",font=("Bahnschrift", 28, "bold"))
     heading.place(x=325,y=20)
@@ -117,7 +149,7 @@ def login_page():
 
 
     #Message box for the login message
-    def on_agree(variable, window):
+    def on_agree(variable):
         if variable.get() == "on" and username_entry.get() !='' and pass_entry.get() != '':
             if admin_login():
                 welcome_page()
@@ -130,7 +162,7 @@ def login_page():
         elif variable.get() == "off":
             messagebox.showwarning("Warning", "You must agree to the terms to continue.")
     submit_button =CTkButton(page_frame, text="Agree and Continue",
-                                  command=lambda: on_agree(agree_var,page_frame))
+                                  command=lambda: on_agree(agree_var))
     submit_button.place(x=350, y=440)
 
 #--------------------------------------------------------
@@ -145,15 +177,13 @@ def otp_confirm_btn(confirm_otp):
                                corner_radius=10, placeholder_text="Enter your otp")
         otp_entry.place(x=470, y=150)
 
-signup_page_btn=CTkButton(master=taz,text='Sign-Up',font=('Arial',20,'bold'),text_color='white',width=120,height=40,
-                          corner_radius=10,border_width=2,
-                         border_color='white',fg_color='#1F6AA5',command=lambda: switch_option(switch_to='signup'))
-signup_page_btn.place(x=800,y=100)
+
 #--------------------------------------------------------
 
 
 #----------------- Signup Page --------------------
 def signup_page():
+    login_sign()
     heading=CTkLabel(master=page_frame, text="Signup Page",font=("Bahnschrift", 28, "bold"))
     heading.place(x=325,y=20)
 
@@ -194,7 +224,7 @@ def signup_page():
 
 
     #Message box for the login message
-    def on_agree(variable, window):
+    def on_agree(variable):
         if variable.get() == "on" and email_entry.get() != '' and user_entry.get() !='' and pass_entry.get() != '' and confirm_entry.get() != '' and pass_entry.get()==confirm_entry.get():
             email=email_entry.get().strip()
 
@@ -214,10 +244,21 @@ def signup_page():
             messagebox.showwarning("Warning", "You must agree to the terms to continue.")
         elif pass_entry.get() != confirm_entry.get():
             messagebox.showwarning("Warning", "Both password should be similar!!!")
-    submit_button =CTkButton(page_frame, text="Agree and Register",command=lambda: on_agree(agree_var, page_frame))
+    submit_button =CTkButton(page_frame, text="Agree and Register",command=lambda: on_agree(agree_var))
     submit_button.place(x=350, y=440)
 
-login_page()
+
+#--------------------------------------------------------
+
+#----------------------- Menu Show Page---------------------------------
+
+def welcome_page():
+    for data in taz.winfo_children():
+        data.destroy()
+
+    hotel_heading()
+    menu_page()
+
 #--------------------------------------------------------
 
 
@@ -231,7 +272,6 @@ def info_insert():
     val = (username, email)
     mycursor.execute(que, val)
     data=mycursor.fetchone()
-    print(data)
 
     if data == None:
         que = "insert into user_info (email_id,user_name,pass_id) values(%s,%s,%s)"
@@ -239,8 +279,11 @@ def info_insert():
         mycursor.execute(que, value)
         connection.commit()
         messagebox.showinfo("Success", "User successfully registered!")
-        for data1 in page_frame.winfo_children():
+        for data1 in taz.winfo_children():
+
             data1.destroy()
+        usernameVar.set("")
+        passwordVar.set("")
         login_page()
         signup_page_btn.configure(fg_color="gray", text_color="black")
         login_page_btn.configure(fg_color="#1F6AA5", text_color="white", border_color='white')
@@ -263,22 +306,206 @@ def admin_login():
     if data != None:
         for data in taz.winfo_children():
             data.destroy()
+        usernameVar.set('')
+        passwordVar.set('')
         return True
     else:
         messagebox.showerror("Error","Either the username or password is incorrect")
+    usernameVar.set('')
+    passwordVar.set('')
 #--------------------------------------------------------
 
 
 #------------------------- WELCOME PAGE -------------------------------
-def welcome_page():
-    hotel_heading()
+def menu_page():
+    # hotel_heading()
+    global menu_btn
+    menu_btn=CTkButton(master=taz,text='Menu',width=50,height=50,border_color='white',fg_color='transparent',hover_color='#383838',
+                       command=menu_frames)
+    menu_btn.place(x=10,y=80)
+
+#--------------------------------------------------------
+
+#-------------------------- Logout section------------------------------
+def logout_btn():
+    for data in taz.winfo_children():
+        data.destroy()
+    menu_btn.destroy()
+    hide_menu_frame()
+    login_page()
+#--------------------------------------------------------
+
+#--------------------------- Menu Frame-----------------------------
+def hide_menu_frame():
+    if menu_frame is not None and menu_frame.winfo_exists():
+        menu_frame.pack_forget()
+
+def menu_frames():
+
+    global menu_frame
+
+    if menu_frame is not None and menu_frame.winfo_exists():
+        menu_frame.pack(
+            side="left",
+            fill="y",
+            padx=15,
+            pady=15
+        )
+        return
+
+
+    menu_frame = CTkFrame(master=taz, width=300, height=790,corner_radius=20)
+    menu_frame.pack(side="left",fill='y',padx=15,pady=15)
+
+
+
+    menu_exbtn = CTkButton(master=menu_frame, text='exit', width=25, height=25, border_color='white',
+                           fg_color='transparent',
+                           hover_color='#383838',
+                           command=hide_menu_frame)
+    menu_exbtn.place(x=15, y=20)
+
+
+    account_frame=CTkFrame(master=menu_frame, width=280, height=150,fg_color="#383838",corner_radius=10)
+    account_frame.grid(row=0,column=0, padx=15,pady=50)
+    photo_frame=CTkFrame(master=account_frame,width=100,height=100,corner_radius=100)
+    photo_frame.place(x=10,y=10)
+
+
+    btn_frame = CTkFrame(master=menu_frame, width=260, height=450, corner_radius=20)
+    btn_frame.grid(row=1,column=0,padx=15,pady=15)
+
+    btn_frame_label = CTkLabel(master=btn_frame, text="| Your Preferences",font=('Microsoft JhengHei',15,'bold'),fg_color="#404040",text_color="#81C784")
+    btn_frame_label.grid(row=1,column=0,pady=15)
+
+
+    add_food=CTkButton(master=btn_frame,text="Add item to the menu",width=260,font=('Microsoft YaHei UI',15),hover_color='#303030',
+                       fg_color='transparent',command=insert_food)
+    add_food.grid(row=2,column=0,pady=15)
+
+    view_order = CTkButton(master=btn_frame, text="See the order", width=260, font=('Microsoft YaHei UI', 15),
+                         hover_color='#303030', fg_color='transparent')
+    view_order.grid(row=3, column=0, pady=15)
+
+    view_emp = CTkButton(master=btn_frame, text="See the Employees", width=260, font=('Microsoft YaHei UI', 15),
+                           hover_color='#303030', fg_color='transparent')
+    view_emp.grid(row=4, column=0, pady=15)
+
+    home_page = CTkButton(master=btn_frame, text="Home Page", width=260, font=('Microsoft YaHei UI', 15),
+                         hover_color='#303030', fg_color='transparent',command=welcome_page)
+    home_page.grid(row=5, column=0, pady=15)
+
+    #Accessebility
+
+    more_frame = CTkFrame(master=menu_frame, width=260, height=450, corner_radius=20)
+    more_frame.grid(row=2, column=0, padx=15, pady=15)
+
+    acc_label = CTkLabel(master=more_frame, text="| More", font=('Microsoft JhengHei', 15, 'bold'),
+                               fg_color="#404040", text_color="#81C784")
+    acc_label.grid(row=1, column=0, pady=15)
+
+    # setting_btn = CTkButton(master=more_frame, text="Setting", width=260, font=('Microsoft YaHei UI', 15),
+    #                      hover_color='#303030',
+    #                      fg_color='transparent')
+    # setting_btn.grid(row=2, column=0, pady=15)
+
+    log_out = CTkButton(master=more_frame, text="Log-out", width=260, font=('Microsoft YaHei UI', 15),
+                           hover_color='#303030', fg_color='transparent',command=logout_btn)
+    log_out.grid(row=2, column=0, pady=15)
+#--------------------------------------------------------
+
+#------------------------- Insert DB-------------------------------
+def insertFood_db():
+    db_config()
+    itemname=itemnameVar.get()
+    itemhprice=int(hpriceVar.get())
+    itemfprice=int(fpriceVar.get())
+    itemcategory=selectOptionVar.get()
+
+    query="insert into item_info (item_name,half_price,full_price,category) values(%s,%s,%s,%s)"
+    value=(itemname,itemhprice,itemfprice,itemcategory)
+    mycursor.execute(query,value)
+    connection.commit()
+    messagebox.showinfo("Success", "Item inserted successfully")
+
+    itemnameVar.set("")
+    hpriceVar.set("")
+    fpriceVar.set("")
+    selectOptionVar.set("Select food type")
+
+
+#--------------------------------------------------------
+#-------------------------- Insert Item ------------------------------
+
+def insert_food():
+    for data in taz.winfo_children():
+        data.destroy()
+    menu_frame=None
+    welcome_page()
+    page_frame = CTkFrame(master=taz, width=800, height=550, corner_radius=10)
+    page_frame.place(relx=0.5, rely=0.55, anchor="center")
+
+    heading = CTkLabel(master=page_frame, text="Insert Item", font=("Bahnschrift", 28, "bold"))
+    heading.place(x=325, y=20)
+
+    item_label = CTkLabel(master=page_frame, text="Add Item :", font=("Bahnschrift", 15, "bold"))
+    item_label.place(x=210, y=105)
+    item_entry = CTkEntry(master=page_frame, width=300, height=40, border_width=2, border_color='gray',
+                           corner_radius=10,
+                           placeholder_text="Enter the food name", textvariable=itemnameVar)
+    item_entry.place(x=300, y=100)
+
+    hprice_label = CTkLabel(master=page_frame, text="Half Price :", font=("Bahnschrift", 15, "bold"))
+    hprice_label.place(x=200, y=205)
+    hprice_entry = CTkEntry(master=page_frame, width=300, height=40, border_width=2, border_color='gray',
+                          corner_radius=10, placeholder_text="Enter price of half", textvariable=hpriceVar)
+    hprice_entry.place(x=300, y=200)
+
+    fprice_label = CTkLabel(master=page_frame, text="Full Price :", font=("Bahnschrift", 15, "bold"))
+    fprice_label.place(x=200, y=265)
+    fprice_entry = CTkEntry(master=page_frame, width=300, height=40, border_width=2, border_color='gray',
+                          corner_radius=10,
+                          placeholder_text="Enter price of full", textvariable=fpriceVar)
+    fprice_entry.place(x=300, y=260)
+
+    category_label = CTkLabel(master=page_frame, text="Category :", font=("Bahnschrift", 15, "bold"))
+    category_label.place(x=200, y=325)
+
+    categoryVar=StringVar(value="Select food type")
+
+    category_entry = CTkOptionMenu(master=page_frame, values=["Indian", "Chinese", "South Indian", "Beverages", "Desert", "Pizza"],
+                                   font=("Bahnschrift", 15, "bold"), width=200, height=40,fg_color="#303030",
+                                   dropdown_hover_color="#303030",variable=categoryVar)
+
+    category_entry.place(x=300, y=325)
+
+
+    def insert_agree():
+        global selectOptionVar
+        selectOptionVar.set(categoryVar.get())
+        if selectOptionVar.get()!="Select food type" and item_entry.get() !='' and hprice_entry.get() !='' and fprice_entry.get() !='' and int(hprice_entry.get()) < int(fprice_entry.get()):
+            insertFood_db()
+
+        elif item_entry.get() == '':
+            messagebox.showwarning("Warning", "Enter the item name!")
+        elif hpriceVar.get() == '':
+            messagebox.showwarning("Warning", "Initialize the half price!")
+        elif fprice_entry.get() == '':
+            messagebox.showwarning("Warning", "Initialize the full price!")
+        elif selectOptionVar.get() == "Select food type":
+            messagebox.showwarning("Warning", "Choose the food category!")
+
+
+    submit_button = CTkButton(page_frame, text="Agree and Register", command=lambda: insert_agree())
+    submit_button.place(x=350, y=440)
 
 
 #--------------------------------------------------------
 
-
 #------------------------ Main component--------------------------------
 login_page()
+
+# menu_frames()
 taz.after(100, lambda: taz.state("zoomed"))
 taz.mainloop()
 #--------------------------------------------------------
